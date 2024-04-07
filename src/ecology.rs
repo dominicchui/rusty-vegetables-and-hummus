a021babdb566fa0dc22477326807776585b60823
Dominic Chui <dominic.chui@outlook.com>
2024-04-06 22:26:50 -0400

    Refactor to flatten structs and improve biomass calculations

--- src/ecology.rs
+++ src/ecology.rs
@@ -1,5 +1,5 @@
 use crate::constants;
-use std::ops::{Index, IndexMut};
+use std::{f32::consts::E, ops::{Index, IndexMut}};
 
 pub struct Ecosystem {
     // Array of structs
@@ -29,54 +29,39 @@ pub(crate) struct Cell {
     pub(crate) soil_moisture: f32,
     pub(crate) sunlight: f32,
     pub(crate) temperature: f32,
-    // pub(crate) side_length: f32,
 }
 
-pub(crate) struct CellLayer {
-    pub(crate) layer_type: CellLayerKind,
-}
-
-pub(crate) enum CellLayerKind {
+pub(crate) enum CellLayer {
     Bedrock(Bedrock),
-    GranularMaterial(GranularMaterial),
-    Vegetation(Vegetation),
-    DeadVegetation(DeadVegetation),
-}
-
-struct GranularMaterial {
-    layer_type: GranularMaterialKind,
-    height: f32,
-    critical_angle: f32,
-}
-
-enum GranularMaterialKind {
     Rock(Rock),
     Sand(Sand),
     Humus(Humus),
+    Trees(Trees),
+    Bushes(Bushes),
+    Grasses(Grasses),
+    DeadVegetation(DeadVegetation),
 }
 
 struct Bedrock {
     height: f32,
 }
 
-struct Rock {}
-
-struct Sand {}
-
-struct Humus {}
-
-pub(crate) struct Vegetation {
-    pub(crate) vegetation_kind: VegetationKind,
+struct Rock {
+    height: f32,
 }
 
-pub(crate) enum VegetationKind {
-    Trees(Trees),
-    Bushes(Bushes),
-    Grasses(Grasses),
+struct Sand {
+    height: f32,
 }
+
+struct Humus {
+    height: f32,
+}
+
 
 pub(crate) struct Trees {
     pub(crate) number_of_plants: u32,
+    // height ∝ diameter ^ (2/3)
     pub(crate) plant_height_sum: f32,
     pub(crate) plant_age_sum: f32,
 }
@@ -92,7 +77,7 @@ struct Grasses {
 }
 
 pub(crate) struct DeadVegetation {
-    pub(crate) volume: f32,
+    pub(crate) biomass: f32, // in kg
 }
 
 // Maybe this should be a static of some sort? It captures the nature of a given type of plant that holds for all types
@@ -108,24 +93,12 @@ struct Plant {
     // etc...
 }
 
-// // list of all possible base layers
-// enum BaseLayerKind {
-//     Bedrock(Bedrock),
-//     Rock(Rock),
-//     Sand(Sand),
-//     Humus(Humus),
-//     Trees(Trees),
-//     Bushes(Bushes),
-//     Grasses(Grasses),
-// }
-
 impl Cell {
+
     pub fn get_trees_layer(&self) -> Option<&Trees> {
         for layer in &self.layers {
-            if let CellLayerKind::Vegetation(vegetation) = &layer.layer_type {
-                if let VegetationKind::Trees(trees) = &vegetation.vegetation_kind {
-                    return Some(trees);
-                }
+            if let CellLayer::Trees(trees) = layer {
+                return Some(trees);
             }
         }
         None
@@ -133,10 +106,8 @@ impl Cell {
 
     pub fn get_trees_layer_mut(&mut self) -> Option<&mut Trees> {
         for layer in &mut self.layers {
-            if let CellLayerKind::Vegetation(vegetation) = &mut layer.layer_type {
-                if let VegetationKind::Trees(trees) = &mut vegetation.vegetation_kind {
-                    return Some(trees);
-                }
+            if let CellLayer::Trees(trees) = layer {
+                return Some(trees);
             }
         }
         None
@@ -144,7 +115,7 @@ impl Cell {
 
     pub fn get_dead_vegetation_layer(&self) -> Option<&DeadVegetation> {
         for layer in &self.layers {
-            if let CellLayerKind::DeadVegetation(dead_vegetation) = &layer.layer_type {
+            if let CellLayer::DeadVegetation(dead_vegetation) = layer {
                 return Some(dead_vegetation);
             }
         }
@@ -153,79 +124,96 @@ impl Cell {
 
     pub fn get_dead_vegetation_layer_mut(&mut self) -> Option<&mut DeadVegetation> {
         for layer in &mut self.layers {
-            if let CellLayerKind::DeadVegetation(dead_vegetation) = &mut layer.layer_type {
+            if let CellLayer::DeadVegetation(dead_vegetation) = layer {
                 return Some(dead_vegetation);
             }
         }
         None
     }
 
-    pub fn add_dead_vegetation(&mut self, volume: f32) {
+    pub fn add_dead_vegetation(&mut self, biomass: f32) {
         if let Some(dead_vegetation) = self.get_dead_vegetation_layer_mut() {
-            dead_vegetation.volume += volume;
+            dead_vegetation.biomass += biomass;
         } else {
             // add new layer
-            let dead_vegetation = DeadVegetation { volume };
-            let layer = CellLayer {
-                layer_type: CellLayerKind::DeadVegetation(dead_vegetation),
-            };
+            let dead_vegetation = DeadVegetation { biomass };
+            let layer = CellLayer::DeadVegetation(dead_vegetation);
             self.layers.push(layer);
         }
     }
 
-    // "an average green wood weight (m in kg / m3), ... bio-volume (b), ... average plant height (h): b = 0.52h^(2.55) / m"
-    pub fn get_tree_biovolume(&self) -> f32 {
-        let mut biovolume = 0.0;
+    pub fn get_tree_biomass(&self) -> f32 {
+        let mut biomass = 0.0;
         // assume max one tree layer
         if let Some(trees) = self.get_trees_layer() {
-            biovolume += trees.get_tree_biovolume();
+            biomass += trees.get_tree_biomass();
         }
-        biovolume
+        biomass
     }
 }
 
 impl Trees {
-    pub fn get_tree_biovolume(&self) -> f32 {
-        self.number_of_plants as f32 * (0.52 * f32::powf(self.plant_height_sum / self.number_of_plants as f32, 2.55) / constants::AVERAGE_GREEN_WOOD_WEIGHT)
+    pub fn get_tree_biomass(&self) -> f32 {
+        // based on biomass equation for red maples
+        // source: https://academic.oup.com/forestry/article/87/1/129/602137#9934369
+        // biomass in kg = e ^ (-2.0470 + 2.3852*ln(diameter in cm))
+        let average_height = self.plant_height_sum / self.number_of_plants as f32;
+        let average_diameter = Trees::estimate_diameter_from_height(average_height);
+        let average_biomass = f32::powf(E, -2.0470 + 2.3852 * f32::ln(average_diameter));
+        average_biomass * self.number_of_plants as f32
+    }
+
+    pub fn estimate_diameter_from_height(height: f32) -> f32 {
+        // based on red maples
+        // source: https://www.ccsenet.org/journal/index.php/jps/article/view/69956
+        // log(height in m) = 0.6 * log(diameter in cm) - 0.4
+        f32::powf(10.0, (f32::log10(height) - 0.6) / 0.4)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::ecology::{Cell, CellLayer, CellLayerKind, Trees, Vegetation, VegetationKind};
+    use float_cmp::approx_eq;
+
+    use crate::ecology::{Cell, CellLayer, Trees};
     #[test]
     fn get_tree_biovolume() {
-        let layer = CellLayer {
-            layer_type: CellLayerKind::Vegetation(Vegetation {
-                vegetation_kind: VegetationKind::Trees(Trees {
-                    number_of_plants: 1,
-                    plant_height_sum: 30.0,
-                    plant_age_sum: 10.0,
-                }),
-            }),
-        };
+        let layer = CellLayer::Trees(Trees {
+            number_of_plants: 1,
+            plant_height_sum: 10.0,
+            plant_age_sum: 10.0
+        });
         let mut cell = Cell {
             layers: vec![layer],
             soil_moisture: 0.0,
             sunlight: 0.0,
             temperature: 0.0,
         };
-        let volume = cell.get_tree_biovolume();
-        let expected = 3.0385225;
+        let volume = cell.get_tree_biomass();
+        let expected = 31.3472318147;
         assert!(
-            volume == expected,
+            approx_eq!(f32,
+            volume, expected, epsilon = 0.001),
             "Expected volume {expected}, actual volume {volume}"
         );
 
         if let Some(trees) = cell.get_trees_layer_mut() {
             trees.number_of_plants = 5;
-            trees.plant_height_sum = 150.0;
+            trees.plant_height_sum = 50.0;
         }
-        let volume = cell.get_tree_biovolume();
-        let expected = 15.1926125;
+        let volume = cell.get_tree_biomass();
+        let expected = 156.7361590735;
         assert!(
-            volume == expected,
+            approx_eq!(f32,
+                volume, expected, epsilon = 0.001),
             "Expected volume {expected}, actual volume {volume}"
         );
+    }
+
+    #[test]
+    fn estimate_diameter_from_height() {
+        let estimate = Trees::estimate_diameter_from_height(10.0);
+        let expected = 10.0;
+        assert!(approx_eq!(f32, expected, estimate, epsilon = 0.00001), "Expected {expected}; actual {estimate}");
     }
 }
--- src/events.rs
+++ src/events.rs
@@ -60,7 +60,7 @@ impl Events {
     // converts all trees in a cell into dead vegetation
     fn kill_trees(cell: &mut Cell) {
         if let Some(trees) = cell.get_trees_layer_mut() {
-            let biovolume = trees.get_tree_biovolume();
+            let biovolume = trees.get_tree_biomass();
             trees.number_of_plants = 0;
             trees.plant_height_sum = 0.0;
             trees.plant_age_sum = 0.0;
@@ -71,25 +71,21 @@ impl Events {
 
 #[cfg(test)]
 mod tests {
-    use crate::{ecology::{Cell, CellLayer, CellLayerKind, Trees, Vegetation, VegetationKind}, events::Events};
+    use crate::{ecology::{Cell, CellLayer, Trees}, events::Events};
     #[test]
     fn kill_trees() {
-        let layer = CellLayer {
-            layer_type: CellLayerKind::Vegetation(Vegetation {
-                vegetation_kind: VegetationKind::Trees(Trees {
-                    number_of_plants: 1,
-                    plant_height_sum: 30.0,
-                    plant_age_sum: 10.0,
-                }),
-            }),
-        };
+        let layer = CellLayer::Trees(Trees {
+            number_of_plants: 1,
+            plant_height_sum: 30.0,
+            plant_age_sum: 10.0
+        });
         let mut cell = Cell {
             layers: vec![layer],
             soil_moisture: 0.0,
             sunlight: 0.0,
             temperature: 0.0,
         };
-        let biovolume = cell.get_tree_biovolume();
+        let biomass = cell.get_tree_biomass();
 
         Events::kill_trees(&mut cell);
         
@@ -103,13 +99,13 @@ mod tests {
         let dead_vegetation = cell.get_dead_vegetation_layer();
         assert!(dead_vegetation.is_some());
         let dead_vegetation = dead_vegetation.unwrap();
-        assert!(dead_vegetation.volume == biovolume);
+        assert!(dead_vegetation.biomass == biomass);
 
         // add more trees and kill them
         let trees = cell.get_trees_layer_mut().unwrap();
         trees.number_of_plants = 5;
         trees.plant_height_sum = 150.0;
-        let biovolume_2 = cell.get_tree_biovolume();
+        let biomass_2 = cell.get_tree_biomass();
 
         Events::kill_trees(&mut cell);
 
@@ -123,6 +119,6 @@ mod tests {
         let dead_vegetation = cell.get_dead_vegetation_layer();
         assert!(dead_vegetation.is_some());
         let dead_vegetation = dead_vegetation.unwrap();
-        assert!(dead_vegetation.volume == biovolume + biovolume_2);
+        assert!(dead_vegetation.biomass == biomass + biomass_2);
     }
 }
