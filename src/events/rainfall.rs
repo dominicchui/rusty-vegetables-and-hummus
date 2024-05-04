7242821d688733791e086f98fe0bdf37d8c4d112
Ianpyrider <Ianpyrider@users.noreply.github.com>
2024-05-04 18:59:03 -0400

    rainfall kinda works?!

--- src/events/rainfall.rs
+++ src/events/rainfall.rs
@@ -11,16 +11,16 @@ use rand::distributions::WeightedIndex;
 
 impl Events {
     pub(crate) fn apply_rainfall_event(ecosystem: &mut Ecosystem, index: CellIndex) -> Option<(Events, CellIndex)> {
-        let water_level: f32 = 0.001*ecosystem[index].get_height();
+        let water_level: f32 = 0.0001*ecosystem[index].get_height();
 
         //TODO: Account for plants intercepting rainfall
 
-        Self::runoff(ecosystem, index, index, water_level, 0);
+        Self::runoff(ecosystem, index, water_level, [0.0, 0.0, 0.0], 0);
 
         None
     }
 
-    fn runoff(ecosystem: &mut Ecosystem, index: CellIndex, prev: CellIndex, water_level: f32, steps: usize) -> () {
+    fn runoff(ecosystem: &mut Ecosystem, index: CellIndex, water_level: f32, lifted_material: [f32; 3], steps: usize) -> () {
         let neighbors: [Option<CellIndex>; 8] = Cell::get_neighbors(&index).as_array();
         const NUM_NEIGHBORS: usize = 8;
 
@@ -38,17 +38,20 @@ impl Events {
 
             let slope: f32 = ecosystem.get_slope_between_points(index, neighbor);
 
-            if (slope > 0.0 && neighbor != prev) {
+            if (slope > 0.0) {
                 slopes.push(slope);
                 existing_neighbors.push(neighbor);
             }
         }
 
-        //Decide which cell the water will flow to
-        let chosen_slope: f32;
-        let next_cell_index: CellIndex;
-        
+        let cur_cell = &mut ecosystem[index];
+
         if slopes.len() != 0 {
+
+            //Decide which cell the water will flow to
+            let chosen_slope: f32;
+            let next_cell_index: CellIndex;
+
             let dist = WeightedIndex::new(&slopes).unwrap();
             let mut rng = rand::thread_rng();
 
@@ -56,73 +59,84 @@ impl Events {
 
             chosen_slope = slopes[choice];
             next_cell_index = existing_neighbors[choice];
-
-            //Soil absorption
-
-            let cur_cell = &mut ecosystem[index];
-
-            println!("Moved water from {index} to {next_cell_index}");
-
-            //ecosystem[next_cell].soil_moisture += chosen_slope;
-            
+                
             //Erosion
 
-            let sediment_capacity: f32 = constants::KC*water_level; //CS
-            let h_amt = cur_cell.get_humus_height();
-            let r_amt = cur_cell.get_rock_height();
-            let s_amt = cur_cell.get_sand_height();
+            let mut lifted = lifted_material; //**SUM OF THIS** is STV
 
-            let cur_cell_sediment: f32 = h_amt+r_amt+s_amt; //SV
+            if (chosen_slope > 0.2) { //LIFT HAPPENS
+                
+                let sediment_capacity: f32 = constants::KC*water_level; //CS
 
-            let percent_humus: f32 = h_amt/cur_cell_sediment;
-            let percent_rock: f32 = r_amt/cur_cell_sediment;
-            let percent_sand: f32 = s_amt/cur_cell_sediment;
+                let remaining_capacity = sediment_capacity-(lifted[0]+lifted[1]+lifted[2]);
 
-            if (cur_cell_sediment >= sediment_capacity) {
-                //Equation 2 unneeded
+                let h_amt = cur_cell.get_humus_height();
+                let r_amt = cur_cell.get_rock_height();
+                let s_amt = cur_cell.get_sand_height();
 
-                //Equation 3
-                let cur_cell_sediment_change = cur_cell_sediment-((1.0-constants::KD)*(cur_cell_sediment-sediment_capacity));
+                let cur_cell_sediment: f32 = h_amt+r_amt+s_amt;
 
-                cur_cell.remove_humus(cur_cell_sediment_change*percent_humus);
-                cur_cell.remove_rocks(cur_cell_sediment_change*percent_rock);
-                cur_cell.remove_sand(cur_cell_sediment_change*percent_sand);
+                let percent_humus: f32 = h_amt/cur_cell_sediment;
+                let percent_rock: f32 = r_amt/cur_cell_sediment;
+                let percent_sand: f32 = s_amt/cur_cell_sediment;
 
-                let next_cell = &mut ecosystem[next_cell_index];
+                if cur_cell_sediment >= remaining_capacity && cur_cell_sediment != 0.0 { //SEDIMENT FILLS CAPACITY
+                    cur_cell.remove_humus(remaining_capacity*percent_humus);
+                    cur_cell.remove_rocks(remaining_capacity*percent_rock);
+                    cur_cell.remove_sand(remaining_capacity*percent_sand);
 
-                //Equation 1
-                next_cell.add_humus(sediment_capacity*percent_humus);
-                next_cell.add_rocks(sediment_capacity*percent_rock);
-                next_cell.add_sand(sediment_capacity*percent_sand);
-            } else {
-                //Amount eroded
-                let mut eroded = constants::KS*(sediment_capacity-cur_cell_sediment);
+                    lifted[0] += remaining_capacity*percent_humus;
+                    lifted[1] += remaining_capacity*percent_rock;
+                    lifted[2] += remaining_capacity*percent_sand;
+                } else { //ERODE
+                    //Equation 3: Pick up all sediment
+                    cur_cell.remove_humus(h_amt);
+                    cur_cell.remove_rocks(r_amt);
+                    cur_cell.remove_sand(s_amt);
 
-                println!("Eroded {eroded} of bedrock");
+                    lifted[0] += h_amt;
+                    lifted[1] += r_amt;
+                    lifted[2] += s_amt;
 
-                if (eroded > cur_cell.get_bedrock_height()) {
-                    eroded = cur_cell.get_bedrock_height();
+                    //Now, erode an amount equal to K_s*(the difference between capacity and current amount held)
+
+                    let mut eroded = constants::KS*(sediment_capacity-(lifted[0]+lifted[1]+lifted[2]));
+
+                    if (eroded > cur_cell.get_bedrock_height()) {
+                        eroded = cur_cell.get_bedrock_height();
+                    }
+
+                    //Equation 2
+                    cur_cell.remove_bedrock(eroded);
+
+                    //Equation 1
+                    lifted[1] += eroded;
                 }
+            } else { //DEPOSIT
+                let deposited_humus = constants::KD*lifted[0];
+                let deposited_rock = constants::KD*lifted[1];
+                let deposited_sand = constants::KD*lifted[2];
 
-                //Equation 2
-                cur_cell.remove_bedrock(eroded);
+                cur_cell.add_humus(deposited_humus);
+                cur_cell.add_rocks(deposited_rock);
+                cur_cell.add_sand(deposited_sand);
 
-                //Equation 3
-                cur_cell.remove_humus(h_amt);
-                cur_cell.remove_rocks(r_amt);
-                cur_cell.remove_sand(s_amt); 
-
-                let next_cell = &mut ecosystem[next_cell_index];
-
-                //Equation 1
-                next_cell.add_rocks(r_amt + eroded);
+                lifted[0] -= deposited_humus;
+                lifted[1] -= deposited_rock;
+                lifted[2] -= deposited_sand;
             }
 
-            if (steps < 100) {
-                Self::runoff(ecosystem, next_cell_index, index, water_level, steps + 1);
+            if (steps < 1000) {
+                let h = cur_cell.get_height();
+
+                Self::runoff(ecosystem, next_cell_index, water_level, lifted, steps + 1);
             } else {
-                println!("Finished");
+                println!("1k steps");
             }
+        } else {
+            cur_cell.add_humus(lifted_material[0]);
+            cur_cell.add_rocks(lifted_material[1]);
+            cur_cell.add_sand(lifted_material[2]);
         }
     }
 }
